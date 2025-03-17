63fc83aef404cbc14a2f85d950afe189c6c728eb
LautaroBerT <lautaroabert15@gmail.com>
2025-03-17 18:29:12 -0300

    Funciones agregarAmigo y limpiarCampo agregadas

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function asignarTexto(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo');
+    let nombreAmigo = inputAmigo.value.trim();
+    
+    // Validar que el campo no esté vacío
+    if (nombreAmigo === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+    
+    // Añadir al array
+    amigos.push(nombreAmigo);
+    
+    limpiarCampo();
+    
+    return;
+}
+
+function limpiarCampo() {
+    document.getElementById('amigo').value = '';
+    return;
+}
