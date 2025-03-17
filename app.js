d103baa3bd271619fb6d958993050434dd90a654
LautaroBerT <lautaroabert15@gmail.com>
2025-03-17 18:54:00 -0300

    ActualizarListaAmigos agregado

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function asignarTexto(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
 function agregarAmigo() {
     let inputAmigo = document.getElementById('amigo');
     let nombreAmigo = inputAmigo.value.trim();
@@ -20,12 +14,33 @@ function agregarAmigo() {
     // Añadir al array
     amigos.push(nombreAmigo);
     
+    actualizarListaAmigos();
     limpiarCampo();
-    
+
     return;
 }
 
 function limpiarCampo() {
     document.getElementById('amigo').value = '';
+
     return;
 }
+
+function actualizarListaAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    
+    listaAmigos.innerHTML = '';
+    
+    for (let i = 0; i < amigos.length; i++) {
+        // Crear un nuevo elemento de lista para cada amigo
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        
+        // Agregar el elemento a la lista
+        listaAmigos.appendChild(li);
+    }
+
+    console.log(amigos);
+    return;
+}
+
